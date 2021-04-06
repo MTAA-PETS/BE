2c0cbed242c6abe3c4b7ce6427340db854a6dc91
Lucia Mandová <lucka.mandova@gmail.com>
2021-04-06 19:04:56 +0200

    Updated cors policy
    Co-Authored-By: MartinOpatovsky <56114781+MartinOpatovsky@users.noreply.github.com>

--- pets/settings.py
+++ pets/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'petsApp'
 ]
 
@@ -35,6 +36,7 @@ PASSWORD_HASHERS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -102,4 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 django_heroku.settings(locals())
