30ced9f8849ea1b47d2f4819f8175a87e106c03e
Lucia Mandová <lucka.mandova@gmail.com>
2021-04-06 20:38:32 +0200

    Still struggling with corse :)
    Co-Authored-By: MartinOpatovsky <56114781+MartinOpatovsky@users.noreply.github.com>

--- petsApp/views.py
+++ petsApp/views.py
@@ -29,8 +29,13 @@ def getUser(request, id):
         name = pet.name
         res.append({"id:": i, "name:": name,
                     "date:": invoice.date, "amount:": invoice.amount})
-    return JsonResponse({"nick:": u.nick, "email": u.email, "birth": u.birth, "invoices": res}, status=200, safe=False)
-
+    response = JsonResponse({"nick:": u.nick, "email": u.email,
+                             "birth": u.birth, "invoices": res}, status=200, safe=False)
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+    return response
 # REGISTRÁCIA
 
 
